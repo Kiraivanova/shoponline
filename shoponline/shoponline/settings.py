2bb627b21682b48445740678888bcb239775b11a
Kiraivanova <kiraivanov.ivanova@yandex.ru>
2021-09-15 01:11:25 +0300

    shoponlines

--- shoponline/shoponline/settings.py
+++ shoponline/shoponline/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,6 +134,7 @@ STATICFILES_DIRS = (
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
